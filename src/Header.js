936d9dba6e87018197da775c8fd1127632b1bdfa
JP Angelle <jp@bestow.co>
2020-02-16 14:09:56 -0600

    add codesandbox link

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,27 @@ import React from 'react';
 export const Header = () => {
   return (
     <div className="header">
-      <a href="https://github.com/jpangelle/formik-demo">
-        <img alt="github logo" src="https://i.imgur.com/9QNnBgG.png" />
+      <a
+        href="https://github.com/jpangelle/formik-demo"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          alt="github logo"
+          className="github"
+          src="https://i.imgur.com/RtIBYaN.png"
+        />
+      </a>
+      <a
+        href="https://codesandbox.io/s/formik-demo-ws1fm"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          alt="codesandbox logo"
+          className="codesandbox"
+          src="https://i.imgur.com/CwfgbS8.png"
+        />
       </a>
     </div>
   );
